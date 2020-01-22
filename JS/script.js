f0eeea03f5384d9d75167dafd247561eb09a789c
Nidheep <eg.nidheep.10@gmail.com>
2020-01-23 01:37:44 +0530

    updated nav bar

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,7 @@
-$(document).ready(function () {
-    $(".menu-icon").on("click", function () {
-        $("nav ul").toggleClass("showing");
-    });
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
 });
-
-// Scrolling Effect
-
-$(window).on("scroll", function () {
-    if ($(window).scrollTop()) {
-        $('nav').addClass('black');
-    } else {
-        $('nav').removeClass('black');
-    }
-})
